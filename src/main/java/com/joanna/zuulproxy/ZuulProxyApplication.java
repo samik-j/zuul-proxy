35254f69f5cc1c813007c117bd8e5e1bc35d1196
samik-j <jo.senczuk@gmail.com>
2020-07-22 16:26:38 +0200

    Add swagger

--- src/main/java/com/joanna/zuulproxy/ZuulProxyApplication.java
+++ src/main/java/com/joanna/zuulproxy/ZuulProxyApplication.java
@@ -3,7 +3,14 @@ package com.joanna.zuulproxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulProxyApplication {
@@ -12,4 +19,12 @@ public class ZuulProxyApplication {
 		SpringApplication.run(ZuulProxyApplication.class, args);
 	}
 
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
 }
